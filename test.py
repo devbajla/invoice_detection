9fa01c3097413e70f5dd18ce1e088affcdbfc06f
devbajla <91225849+devbajla@users.noreply.github.com>
2023-06-13 17:21:26 +0500

    Add files via upload

--- test.py
+++ test.py
@@ -1,7 +1,19 @@
 import cv2
+import numpy as np
 
-img = cv2.imread('assets/invoice_1.jpg', 0)
-img = cv2.resize(img, (600,600))
-cv2.imshow('image',img)
+img = cv2.imread('assets/invoice_1.jpg')
+word = img[196:228,1663:1730]
+word = cv2.resize(word,(0,0), fx= 4, fy = 4)
+gray = cv2.cvtColor(word, cv2.COLOR_BGR2GRAY)
+
+
+corners = cv2.goodFeaturesToTrack(gray,50, 0.01,10)
+corners = np.int0(corners)
+
+for corner in corners: 
+    x,y = corner.ravel()
+    cv2.circle(word,(x,y), 1, (0, 0, 255), -1)
+
+cv2.imshow('word',word)
 cv2.waitKey(0)
-cv2.destroyAllWindows
+cv2.destroyAllWindows  
